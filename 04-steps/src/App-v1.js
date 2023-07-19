5ab7295a23a17dd2e3bca278bb201fc4a20addf8
sergio bodean <sereoga24@hotmail.com>
2023-07-19 12:37:41 +0100

    small updates

--- 04-steps/src/App-v1.js
+++ 04-steps/src/App-v1.js
@@ -10,6 +10,7 @@ export default function App() {
   return (
     <div>
       <Steps />
+      <Steps />
     </div>
   );
 }
@@ -38,7 +39,10 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
               <span>👈</span> Previous
@@ -61,14 +65,5 @@ function Button({ textColor, bgColor, onClick, children }) {
     >
       {children}
     </button>
-  );
-}
-
-function StepMessage({ step, children }) {
-  return (
-    <div className="message">
-      <h3>Step {step}</h3>
-      {children}
-    </div>
   );
 }
