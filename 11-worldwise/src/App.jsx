3a860caa4378b31fbcded298997e6ca2ec64f23c
sergio bodean <sereoga24@hotmail.com>
2023-11-05 15:10:11 +0100

    added json server, made city list component and city item component

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,38 +1,64 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import AppLayout from './pages/AppLayout';
+import PageNotFound from './pages/PageNotFound';
+import CityList from './components/CityList';
+import { useEffect, useState } from 'react';
+
+const BASE_URL = 'http://localhost:9000';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [cities, setCities] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(function () {
+        async function fetchCities() {
+            try {
+                setIsLoading(true);
+                const res = await fetch(`${BASE_URL}/cities`);
+                const data = await res.json();
+                setCities(data);
+            } catch {
+                alert('there was an error loading data...');
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchCities();
+    }, []);
 
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Homepage />} />
+                <Route path="product" element={<Product />} />
+                <Route path="pricing" element={<Pricing />} />
+                <Route path="login" element={<Login />} />
+                <Route path="app" element={<AppLayout />}>
+                    <Route
+                        index
+                        element={
+                            <CityList cities={cities} isLoading={isLoading} />
+                        }
                     />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+                    <Route
+                        path="cities"
+                        element={
+                            <CityList cities={cities} isLoading={isLoading} />
+                        }
+                    />
+                    <Route
+                        path="countries"
+                        element={<p>List of countries</p>}
+                    />
+                    <Route path="form" element={<p>From</p>} />
+                </Route>
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
